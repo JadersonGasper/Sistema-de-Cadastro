5d4f300a6d35045de141878be705d5680197d082
JadersonGasper <jadersondegasper@gmail.com>
2025-05-04 16:08:04 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+class ControleDeEstoque
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a opção desejada entre: cadastrar, listar, atualizar, remover ou sair");
+        string opcaoEscolhida = Console.ReadLine();
+
+        while (opcaoEscolhida != "sair") {
+            switch (opcaoEscolhida)
+            {
+                case "cadastrar":
+                
+                break;
+            }
+        }
+    }
+}
+
+
+/*using System;
+
+class Testes
+{
+    static void Main(string[] args)
+    {
+        string diaDaSemana = "terça";
+
+        if (diaDaSemana == "terça")
+        {
+            Console.WriteLine("Hoje é terça-feira");
+        }else if (diaDaSemana == "quarta")
+        {
+            Console.WriteLine("Hoje é quarta-feira");
+        }
+        else
+        {
+            Console.WriteLine("Hoje é domingo");
+        }
+    }
+}*/
